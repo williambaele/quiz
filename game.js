f0bb2206b15189e5f553f305d592b03f45d0d483
William Baele <baelewilliam1999@gmail.com>
2023-03-13 09:41:53 +0100

    JS file

---
+++ game.js
@@ -0,0 +1,5 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progessText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
